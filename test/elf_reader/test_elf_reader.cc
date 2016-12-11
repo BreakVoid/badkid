8a89c3544a5d84505bf4c5bb73fe4d3805caeb73
BreakVoid <breakvoid@hotmail.com>
2016-12-11 20:57:40 +0800

    add elf writers

--- test/elf_reader/test_elf_reader.cc
+++ test/elf_reader/test_elf_reader.cc
@@ -8,30 +8,31 @@ int main(int argc, char const *argv[])
 		printf("Open File Failed.\n");
 		return 0;
 	}
-	Elf64_Ehdr *elfHeader = ReadElfHeader(elfFile);
+	Elf64_Ehdr elfHeader;
+	ReadElfHeader(elfFile, &elfHeader);
 
 	const char *wordWidth, *endian;
-	if (elfHeader->e_ident[0] != 0x7F 
-		|| elfHeader->e_ident[1] != 0x45 
-		|| elfHeader->e_ident[2] != 0x4C 
-		|| elfHeader->e_ident[3] != 0x46) {
+	if (elfHeader.e_ident[0] != 0x7F 
+		|| elfHeader.e_ident[1] != 0x45 
+		|| elfHeader.e_ident[2] != 0x4C 
+		|| elfHeader.e_ident[3] != 0x46) {
 		printf("Error: not a elf file.");
 		return 0;
 	}
 
 	int flag = 0;
-	if (elfHeader->e_ident[4] == 0x1) {
+	if (elfHeader.e_ident[4] == 0x1) {
 		wordWidth = "32 bit";
 		flag = 1;
-	} else if (elfHeader->e_ident[4] == 0x2) {
+	} else if (elfHeader.e_ident[4] == 0x2) {
 		wordWidth = "64 bit";
 	} else {
 		wordWidth = "Unknown Word Width";
 		flag = 1;
 	}
-	if (elfHeader->e_ident[5] == 0x1) {
+	if (elfHeader.e_ident[5] == 0x1) {
 		endian = "Little Endian";
-	} else if (elfHeader->e_ident[5] == 0x2) {
+	} else if (elfHeader.e_ident[5] == 0x2) {
 		endian = "Big Endian";
 		flag = 1;
 	} else {
@@ -46,11 +47,12 @@ int main(int argc, char const *argv[])
 		return 0;
 	}
 
-	Elf64_Phdr *elfPhdrTab = ReadElfPhdr(elfFile, elfHeader);
+	Elf64_Phdr *elfPhdrTab = (Elf64_Phdr *) malloc(sizeof(Elf64_Phdr) * elfHeader.e_phnum);
+	ReadElfPhdrTab(elfFile, &elfHeader, elfPhdrTab);
 
 	if (elfPhdrTab) {
-		printf("Number of Programs: %d\n", elfHeader->e_phnum);
-		for (int i = 0; i < elfHeader->e_phnum; ++i) {
+		printf("Number of Programs: %d\n", elfHeader.e_phnum);
+		for (int i = 0; i < elfHeader.e_phnum; ++i) {
 			printf("Program $%d\n", i);
 			printf("\tFile Offset       0x%016llX\n", elfPhdrTab[i].p_offset);
 			printf("\tVirtual Address   0x%016llX\n", elfPhdrTab[i].p_vaddr);
@@ -71,10 +73,11 @@ int main(int argc, char const *argv[])
 		}
 	}
 
-	Elf64_Shdr *elfShdrTab = ReadElfShdr(elfFile, elfHeader);
+	Elf64_Shdr *elfShdrTab = (Elf64_Shdr *) malloc(sizeof(Elf64_Shdr) * elfHeader.e_shnum);
+	ReadElfPhdrTab(elfFile, &elfHeader, elfPhdrTab);
 	if (elfShdrTab) {
-		printf("Number of Sections: %d\n", elfHeader->e_shnum);
-		for (int i = 0; i < elfHeader->e_shnum; ++i) {
+		printf("Number of Sections: %d\n", elfHeader.e_shnum);
+		for (int i = 0; i < elfHeader.e_shnum; ++i) {
 			printf("Sections $%d\n", i);
 		}
 	}
